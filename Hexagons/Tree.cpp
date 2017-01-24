77b09462d61b9e30e64e9d112cd217ce24d9a39f
Seb Russell <sebrussell42@gmail.com>
2017-01-24 21:34:39 +0000

    Still trying to assign values to tree nodes

--- Hexagons/Tree.cpp
+++ Hexagons/Tree.cpp
@@ -1,6 +1,6 @@
 #include "Tree.h"
 #include <iostream>
-
+#include <iterator>
 
 Tree::Tree()
 {
@@ -23,8 +23,8 @@ void Tree::GenerateTree()
 	while (m_nodesToExpand.size() > 0)
 	{
 		if (m_nodesToExpand.front()->GetGameStatus() == GameState::playing)
-		{						
-			for (size_t i = 0; i < 9; i++) {	
+		{
+			for (size_t i = 0; i < 3; i++) {
 				std::string tempString = m_nodesToExpand.front()->GetBoardString();
 				std::shared_ptr<GameState> tempBoard(new GameState);
 				if (tempString[i] == ' ')
@@ -38,32 +38,39 @@ void Tree::GenerateTree()
 					{
 						tempString[i] = 'o';
 						tempBoard->TurnPlaced(false);
-					}					
-					tempBoard->SetBoardString(tempString);	
+					}
+					tempBoard->SetBoardString(tempString);
 					tempBoard->CheckForWin();
 					m_nodesToExpand.front()->AddChild(tempBoard);
 					m_nodesToExpand.push_back(tempBoard);
-				}				
+				}
 			}
-		}	
+		}
 		m_nodesToExpand.erase(m_nodesToExpand.begin());
 	}
+
+	printf("Generated the tree");
 
 	CalculateNodeValues(rootNode);
 }
 
 int Tree::CalculateNodeValues(std::shared_ptr<GameState>& _node)
 {
+	int t = 0;
+
 	if (_node->GetNodeValue() == 0)
 	{
-		for (auto &node : _node->GetChildNodes())
+		for (std::list<std::shared_ptr<GameState>>::iterator& iterator = _node->GetChildNodes().begin(), end = _node->GetChildNodes().end(); iterator != end; ++iterator)
 		{
-			int t = CalculateNodeValues(node);
+			t = CalculateNodeValues(*iterator);
 			_node->SetNodeValue(t);
+			return _node->GetNodeValue();
 		}
 	}
 	else
 	{
 		return _node->GetNodeValue();
 	}
+
+
 }
