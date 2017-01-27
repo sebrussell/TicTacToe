d92c5a38f94f69e362f7ccb215c5510a30afaae8
Seb Russell <sebrussell42@gmail.com>
2017-01-27 15:19:19 +0000

    Added a choice between player or ai starting first

--- Hexagons/Source.cpp
+++ Hexagons/Source.cpp
@@ -48,6 +48,7 @@ int main(int argc, char **argv)															//start of main
 	std::string tempString;
 
 	bool gameOver = false;
+	bool playerToGoFirst = false;
 
 	while (1 == 1)																										//loop through until done
 	{
@@ -55,53 +56,14 @@ int main(int argc, char **argv)															//start of main
 		//Draw();
 		//UserInput();
 
-		if (test.GetNode()->DidXGoLast() == false)
-		{
-			std::cout << "Enter position";
-			tempString = test.GetNode()->GetBoardString();
-			std::cin >> position;
-			tempString[position] = 'x';
-
-			for (std::shared_ptr<GameState> l : test.GetNode()->GetChildNodes())		//change this to actual not 2015 stuff
-			{
-				if (l->GetBoardString() == tempString)
-				{
-					test.SetNode(l);
-					break;
-				}
-			}
-		}
-		else
-		{
-			std::shared_ptr<GameState> tempNode = test.GetNode()->GetChildNodes().front();
-
-			if (test.GetNode()->GetChildNodes().size() > 0)
-			{
-				for (std::shared_ptr<GameState> l : test.GetNode()->GetChildNodes())		//change this to actual not 2015 stuff
-				{
-					if (l->GetNodeValue() < tempNode->GetNodeValue())
-					{
-						tempNode = l;
-					}
-				}
-			}
-			else
-			{
-				gameOver = true;
-			}
-			
-
-			test.SetNode(tempNode);
-		}
-
 		if (gameOver == false)
 		{
+			std::cout << std::endl;
 			std::cout << test.GetNode()->GetBoardString()[0] << "|" << test.GetNode()->GetBoardString()[1] << "|" << test.GetNode()->GetBoardString()[2] << std::endl;
 			std::cout << "-----" << std::endl;
 			std::cout << test.GetNode()->GetBoardString()[3] << "|" << test.GetNode()->GetBoardString()[4] << "|" << test.GetNode()->GetBoardString()[5] << std::endl;
 			std::cout << "-----" << std::endl;
 			std::cout << test.GetNode()->GetBoardString()[6] << "|" << test.GetNode()->GetBoardString()[7] << "|" << test.GetNode()->GetBoardString()[8] << std::endl;
-			std::cout << std::endl;
 			std::cout << std::endl;
 		}
 		else
@@ -109,8 +71,88 @@ int main(int argc, char **argv)															//start of main
 			std::cout << "Game over";
 		}
 
-		
- 		
+		if (playerToGoFirst == true)
+		{
+			if (test.GetNode()->DidXGoLast() == false)
+			{
+				std::cout << "Enter position";
+				tempString = test.GetNode()->GetBoardString();
+				std::cin >> position;
+				tempString[position] = 'x';
+
+				for (std::shared_ptr<GameState> l : test.GetNode()->GetChildNodes())		//change this to actual not 2015 stuff
+				{
+					if (l->GetBoardString() == tempString)
+					{
+						test.SetNode(l);
+						break;
+					}
+				}
+			}
+			else
+			{
+				std::shared_ptr<GameState> tempNode = test.GetNode()->GetChildNodes().front();
+
+				if (test.GetNode()->GetChildNodes().size() > 0)
+				{
+					for (std::shared_ptr<GameState> l : test.GetNode()->GetChildNodes())		//change this to actual not 2015 stuff
+					{
+						if (l->GetNodeValue() < tempNode->GetNodeValue())
+						{
+							tempNode = l;
+						}
+					}
+				}
+				else
+				{
+					gameOver = true;
+				}
+
+
+				test.SetNode(tempNode);
+			}
+		}
+		else
+		{
+			if (test.GetNode()->DidXGoLast() == true)
+			{
+				std::cout << "Enter position";
+				tempString = test.GetNode()->GetBoardString();
+				std::cin >> position;
+				tempString[position] = 'o';
+
+				for (std::shared_ptr<GameState> l : test.GetNode()->GetChildNodes())		//change this to actual not 2015 stuff
+				{
+					if (l->GetBoardString() == tempString)
+					{
+						test.SetNode(l);
+						break;
+					}
+				}
+			}
+			else
+			{
+				std::shared_ptr<GameState> tempNode = test.GetNode()->GetChildNodes().front();
+
+				if (test.GetNode()->GetChildNodes().size() > 0)
+				{
+					for (std::shared_ptr<GameState> l : test.GetNode()->GetChildNodes())		//change this to actual not 2015 stuff
+					{
+						if (l->GetNodeValue() > tempNode->GetNodeValue())
+						{
+							tempNode = l;
+						}
+					}
+				}
+				else
+				{
+					gameOver = true;
+				}
+
+
+				test.SetNode(tempNode);
+			}
+		}		
 		
 		end = clock();
 		deltaTime = double(end - begin) / CLOCKS_PER_SEC;															//this is used for delta time and timers
